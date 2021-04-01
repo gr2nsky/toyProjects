68168e8a9eb50355dc9eb19cb94cc57f44a61c53
gr2nsky <44865986+gr2nsky@users.noreply.github.com>
2021-04-01 23:34:59 +0900

    freensky
    try mysql connect for springboot

---
+++ mysqlConnect/src/main/java/com/example/demo/MysqlConnectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlConnectApplication.class, args);
+	}
+
+
+}
